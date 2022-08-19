745194fbfc5981fcb3541c21ede64abd5f956a5c
Bradford Zhang <zyc@zyc.name>
2022-08-19 12:38:49 +0800

    ci: 🎡 update ci

---
+++ scripts/config.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+echo='echo -e' && [ -n "$(echo -e|grep e)" ] && echo=echo
+
+echo "***********************************************"
+echo "**                 欢迎使用                   **"
+echo "**                minecraftspeedproxy-config **"
+echo "**            配置文件路径默认请使用sudo         **"
+echo "***********************************************"
+
+echo -----------------------------------------------
+$echo " 1 \033[32m正在配置远程服务器地址\033[0m"
+echo -----------------------------------------------
+read -p "请输入远程服务器地址 > " Address
+if [ -z $Address ];then
+	echo 已取消！ && exit 1;
+fi
+echo -----------------------------------------------
+$echo " 2 \033[32m正在配置远程服务器端口\033[0m"
+echo -----------------------------------------------
+read -p "请输入远程服务器端口 > " RemotePort
+if [ -z $RemotePort ];then
+	echo 已取消！ && exit 1;
+fi
+echo -----------------------------------------------
+$echo " 3 \033[32m正在配置本地监听端口\033[0m"
+echo -----------------------------------------------
+read -p "请输入本地监听端口 > " LocalPort
+if [ -z $LocalPort ];then
+	echo 已取消！ && exit 1;
+fi
+echo -----------------------------------------------
+$echo " 3 \033[32m正在配置是否允许输入\033[0m"
+echo -----------------------------------------------
+read -p "请输入true or false > " AllowInput
+if [ -z $AllowInput ];then
+	echo 已取消！ && exit 1;
+fi
+echo -----------------------------------------------
+$echo " 4 \033[32m正在配置配置文件路径(留空为默认)\033[0m"
+echo -----------------------------------------------
+read -p "请输入配置文件路径 > " configpath
+if [ -z $configpath ];then
+	configpath='/etc/minecraftspeedproxy/config.json'
+fi
+echo { \"Address\":\"$Address\", \"RemotePort\":\"$RemotePort\", \"LocalPort\":\"$LocalPort\", \"AllowInput\":\"$AllowInput\" } > $configpath
---
+++ scripts/release.sh
@@ -0,0 +1,41 @@
+version=$(git describe --tags)
+version1=${version#v}
+VERSION=${version1%%-*}
+
+mkdir -p deb/DEBIAN
+mkdir -p deb/lib/systemd/system
+mkdir -p deb/usr/bin
+cp ./minecraftspeedproxy deb/usr/bin
+tee deb/lib/systemd/system/Minecraft-Speed-Proxy.service <<-'EOF'
+[Unit]
+Description=Minecraft speedup program
+
+[Service]
+type=forking
+ExecStart=sudo minecraftspeedproxy -c /etc/minecraftspeedproxy/config.json
+EOF
+
+cd deb/DEBIAN
+touch control postinst postrm
+chmod 755 control postinst postrm
+
+tee control <<-'EOF'
+Package: Minecraft-Speed-Proxy
+EOF
+
+echo Version: $VERSION >> control
+
+tee control -a <<-'EOF'
+Architecture: amd64
+Maintainer: AllesUgo<yangpengxuanab@outlook.com>
+Installed-Size: 1.08M
+Provides: optional
+Section: net
+Description: Minecraft speedup program.
+EOF
+
+echo 'sudo systemctl daemon-reload' >> postinst
+echo 'sudo systemctl daemon-reload' >> postrm
+
+cd ../..
+dpkg -b deb/ Minecraft-Speed-Proxy-linux-amd64.deb
