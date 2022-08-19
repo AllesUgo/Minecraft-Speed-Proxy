424f5f5149404604e23cee54aa6bcd8bf2b5c1d0
mufengXD <mufengXD@outlook.com>
2022-08-19 23:40:37 +0800

    test

--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 #include "websocket.h"
 #include "cJSON.h"
 #include "log.h"
-int a;
 char *Version;
 char *remoteServerAddress;
 const char *Defalut_Configfile_Path = "/etc/minecraftspeedproxy/config.json";
